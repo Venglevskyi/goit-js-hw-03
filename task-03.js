cca7034e1425162634514cb86dd4f1c396521367
Roman <Roman.Vengl1@gmail.com>
2019-09-22 20:52:24 +0300

    task-3

---
+++ task-03.js
@@ -0,0 +1,22 @@
+const findBestEmployee = function productiveWorker(employees) {
+  const entries = Object.entries(employees);
+  let max = 0;
+  let name;
+  for (const entry of entries) {
+    const key = entry[0];
+    const value = entry[1];
+    if (max < value) {
+      max = value;
+      name = key;
+    }
+  }
+  return name;
+};
+
+const worker = {
+  ann: 29,
+  david: 35,
+  helen: 1,
+  lorence: 99,
+};
+console.log(findBestEmployee(worker));
